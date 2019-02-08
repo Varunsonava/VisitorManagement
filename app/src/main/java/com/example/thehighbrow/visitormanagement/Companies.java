32988d00deb6cc294603f0d23e82549e1a0cedc2
TheHighbrow <varunsonava@gmail.com>
2019-02-08 21:36:52 +0530

    added CSV import Support

---
+++ app/src/main/java/com/example/thehighbrow/visitormanagement/Companies.java
@@ -0,0 +1,28 @@
+package com.example.thehighbrow.visitormanagement;
+
+class Companies {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- app/src/main/java/com/example/thehighbrow/visitormanagement/MyFragment.java
+++ app/src/main/java/com/example/thehighbrow/visitormanagement/MyFragment.java
@@ -2,6 +2,7 @@ package com.example.thehighbrow.visitormanagement;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -20,7 +21,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import static android.view.View.VISIBLE;
 
@@ -52,9 +57,22 @@ public class MyFragment extends Fragment {
         View v= null;
         Bundle bundle=getArguments();
         int i = bundle.getInt("pos");
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat date = new SimpleDateFormat("dd-");
+        SimpleDateFormat month = new SimpleDateFormat("MM-");
+        SimpleDateFormat year = new SimpleDateFormat("yyyy");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String formattedDate = dateFormat.format(c);
+        String dates = date.format(c);
+        String months = month.format(c);
+        String years = year.format(c);
+
         if (i==0){
             v = inflater.inflate(R.layout.visitor_fragment,container,false);
-            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1/07-02-2019/visitor");
+            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1");
+
+            //databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1/"+dates+months+years+"/visitor");
+
             recyclerView= v.findViewById(R.id.recyclerView);
             Context context = getContext();
             visitors = new ArrayList<Visitor>();
@@ -81,88 +99,70 @@ public class MyFragment extends Fragment {
 
                     for (DataSnapshot visitorSnapshot : dataSnapshot.getChildren()) {
                         Log.e(TAG, "onDataChange: adding visitor to visitors");
-                        Visitor visitor = visitorSnapshot.getValue(Visitor.class);
-                        visitors.add(visitor);
-                        adapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
+                        if (visitorSnapshot.hasChild("visitor")) {
+                            for (DataSnapshot dataSnapshot1 : visitorSnapshot.child("visitor").getChildren()) {
+                                Visitor visitor = dataSnapshot1.getValue(Visitor.class);
+                                visitors.add(visitor);
+                                adapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
 
-                        Log.e("MainActivity", "onDataChange: added visitor to visitors");
-
+                                Log.e("MainActivity", "onDataChange: added visitor to visitors");
+                            }
+                        }
                     }
                 }
-
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                     Toast.makeText(getActivity(), "Problem fetching databse", Toast.LENGTH_SHORT).show();
-
                 }
             });
-
-
         }
         else if (i==1){
 
-            v = inflater.inflate(R.layout.visitor_fragment,container,false);
-            databaseVisitor = FirebaseDatabase.getInstance().getReference("dayVisitor");
-            recyclerView= v.findViewById(R.id.recyclerView);
+            v = inflater.inflate(R.layout.day_fragment,container,false);
+            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1");
+            recyclerView= v.findViewById(R.id.recyclerViewDay);
             Context context = getContext();
-
             dayVisitors = new ArrayList<DayVisitor>();
-
             Log.e(TAG, "onCreate: above llm");
             LinearLayoutManager mlm = new LinearLayoutManager(getActivity());
             recyclerView.setLayoutManager(mlm);
             mlm.setReverseLayout(true);
             mlm.setStackFromEnd(true);
-
             adapter = new DayAdapter(dayVisitors);
-
             recyclerView.setAdapter(adapter);
             Log.e(TAG, "onCreate: below llm");
-
-
             final View finalV = v;
             databaseVisitor.addValueEventListener(new ValueEventListener() {
-
-
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                     progressBar = finalV.findViewById(R.id.progressBar);
                     progressBar.setVisibility(VISIBLE);
-
-
                     for (DataSnapshot visitorSnapshot : dataSnapshot.getChildren()) {
-                        Log.e(TAG, "onDataChange: adding visitor to visitors");
+                        if (visitorSnapshot.hasChild(" dayVisitor")){
+                            for (DataSnapshot dataSnapshot1:visitorSnapshot.child("dayVisitor").getChildren()){
 
-                        DayVisitor dayVisitor = visitorSnapshot.getValue(DayVisitor.class);
-                        dayVisitors.add(dayVisitor);
-
-                        adapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
-
-                        Log.e("MainActivity", "onDataChange: added visitor to visitors");
-
+                                Log.e(TAG, "onDataChange: adding visitor to visitors");
+                                DayVisitor dayVisitor = dataSnapshot1.getValue(DayVisitor.class);
+                                dayVisitors.add(dayVisitor);
+                                adapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
+                                Log.e("MainActivity", "onDataChange: added visitor to visitors exiting");
+                            }
+                        }
                     }
                 }
-
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
                     Toast.makeText(getActivity(), "Problem fetching databse", Toast.LENGTH_SHORT).show();
-
                 }
             });
-
-
-
         }
         else if (i==2){
-
-
             v = inflater.inflate(R.layout.visitor_fragment,container,false);
-            databaseVisitor = FirebaseDatabase.getInstance().getReference("91lead");
+            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1");
             recyclerView= v.findViewById(R.id.recyclerView);
-            Context context = getContext();
+            final Context context = getContext();
 
             leads = new ArrayList<Lead>();
 
@@ -176,29 +176,28 @@ public class MyFragment extends Fragment {
 
             recyclerView.setAdapter(adapter);
             Log.e(TAG, "onCreate: below llm");
-
-
             final View finalV = v;
             databaseVisitor.addValueEventListener(new ValueEventListener() {
-
-
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                     progressBar = finalV.findViewById(R.id.progressBar);
                     progressBar.setVisibility(VISIBLE);
-
-
                     for (DataSnapshot visitorSnapshot : dataSnapshot.getChildren()) {
-                        Log.e(TAG, "onDataChange: adding visitor to visitors");
+                        if (visitorSnapshot.hasChild("91lead")){
+                            for (DataSnapshot dataSnapshot1: visitorSnapshot.child("91lead").getChildren()){
 
-                        Lead lead = visitorSnapshot.getValue(Lead.class);
-                        leads.add(lead);
+                                Log.e(TAG, "onDataChange: adding visitor to visitors");
+                                Lead lead = dataSnapshot1.getValue(Lead.class);
+                                leads.add(lead);
 
-                        adapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
+                                adapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
 
-                        Log.e("MainActivity", "onDataChange: added visitor to visitors");
+                                Log.e("MainActivity", "onDataChange: added visitor to visitors");
+                            }
+
+
+                        }
 
                     }
                 }
@@ -214,7 +213,7 @@ public class MyFragment extends Fragment {
         else if (i==3){
 
             v = inflater.inflate(R.layout.visitor_fragment,container,false);
-            databaseVisitor = FirebaseDatabase.getInstance().getReference("vendor");
+            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1");
             recyclerView= v.findViewById(R.id.recyclerView);
             Context context = getContext();
 
@@ -245,15 +244,20 @@ public class MyFragment extends Fragment {
 
 
                     for (DataSnapshot visitorSnapshot : dataSnapshot.getChildren()) {
-                        Log.e(TAG, "onDataChange: adding visitor to visitors");
+                        if (visitorSnapshot.hasChild("vendor")){
+                            for (DataSnapshot dataSnapshot1: visitorSnapshot.child("vendor").getChildren()){
+                                Log.e(TAG, "onDataChange: adding visitor to visitors");
 
-                        Vendor vendor = visitorSnapshot.getValue(Vendor.class);
-                        vendors.add(vendor);
+                                Vendor vendor = dataSnapshot1.getValue(Vendor.class);
+                                vendors.add(vendor);
 
-                        adapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
+                                adapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
 
-                        Log.e("MainActivity", "onDataChange: added visitor to visitors");
+                                Log.e("MainActivity", "onDataChange: added visitor to visitors");
+
+                            }
+                        }
 
                     }
                 }
@@ -269,7 +273,7 @@ public class MyFragment extends Fragment {
         else if (i==4){
 
             v = inflater.inflate(R.layout.visitor_fragment,container,false);
-            databaseVisitor = FirebaseDatabase.getInstance().getReference("courier");
+            databaseVisitor = FirebaseDatabase.getInstance().getReference("Noida Sec1");
             recyclerView= v.findViewById(R.id.recyclerView);
             Context context = getContext();
 
@@ -300,15 +304,20 @@ public class MyFragment extends Fragment {
 
 
                     for (DataSnapshot visitorSnapshot : dataSnapshot.getChildren()) {
-                        Log.e(TAG, "onDataChange: adding visitor to visitors");
+                        if (visitorSnapshot.hasChild("courier")){
+                            for (DataSnapshot dataSnapshot1:visitorSnapshot.child("courier").getChildren()){
+                                Log.e(TAG, "onDataChange: adding visitor to visitors");
 
-                        Courier courier = visitorSnapshot.getValue(Courier.class);
-                        couriers.add(courier);
+                                Courier courier = dataSnapshot1.getValue(Courier.class);
+                                couriers.add(courier);
 
-                        adapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
+                                adapter.notifyDataSetChanged();
+                                progressBar.setVisibility(View.GONE);
 
-                        Log.e("MainActivity", "onDataChange: added visitor to visitors");
+                                Log.e("MainActivity", "onDataChange: added visitor to visitors");
+
+                            }
+                        }
 
                     }
                 }
